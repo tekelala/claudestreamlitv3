03aedee509a899a89ea54eff19ab4bb1b1ed0b6d
tekelala <camilo@tekelala.com>
2023-08-17 09:12:45 -0500

    b

--- app.py
+++ app.py
@@ -9,11 +9,9 @@ def send_message(prompts):
         "X-API-Key": st.secrets["API_KEY"]
     }
 
-    # Prepare the prompt for Claude
     last_message = prompts[-1]['content']
     conversation = f"Human: {last_message}\n\nAssistant:"
 
-    # Define the body of the request
     body = {
         "prompt": conversation,
         "model": "claude-2.0",
@@ -22,7 +20,6 @@ def send_message(prompts):
         "stop_sequences": ["\n\nHuman:"]
     }
 
-    # Make a POST request to the Claude API
     try:
         response = requests.post(api_url, headers=headers, data=json.dumps(body))
         response.raise_for_status()
@@ -37,10 +34,7 @@ def send_message(prompts):
     except Exception as e:
         st.error(f"Unexpected error: {e}")
 
-     # Extract Claude's response from the JSON response
     result = response.json()
-
-    # Return Claude's response as a string
     return result['completion'].strip()
 
 st.title("Chat with Claude")
@@ -49,15 +43,17 @@ st.write("Welcome to our chat app!")
 if "prompts" not in st.session_state:
     st.session_state.prompts = []
 
-# Display the entire conversation
+if "new_message" not in st.session_state:
+    st.session_state.new_message = False
+
 for prompt in st.session_state.prompts:
     with st.chat_message(prompt['role']):
         st.write(prompt['content'])
 
-# Capture and display user's message
-user_message = st.chat_input("Say something")
-if user_message:
-    with st.spinner('Writting...'):
+if not st.session_state.new_message:
+    user_message = st.chat_input("Say something")
+    if user_message:
+        st.session_state.new_message = True
         st.session_state.prompts.append({"role": "Human", "content": user_message})
         response_from_claude = send_message(st.session_state.prompts)
         st.session_state.prompts.append({"role": "Assistant", "content": response_from_claude})
@@ -65,4 +61,5 @@ if user_message:
 
 if st.button('Restart'):
     st.session_state.prompts = []
+    st.session_state.new_message = False
     st.experimental_rerun()
